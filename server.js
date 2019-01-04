6ca6a574a2497cdcb408d2895fe2e5906796c83c
cosho <rizeupbass@gmail.com>
2019-01-04 21:16:53 +0900

    1ヶ月制限可変化

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/image_url', function(req, res) {
     console.log(req);
     if (!req.query.tags) req.query.tags = '';
     if (!req.query.limit) req.query.limit = 20;
+    if (!req.query.from) req.query.from = 1;
     //if (req.query.static) res.send(JSON.parse(fs.readFileSync('./sample.json', 'utf8')));
     getMediaUrls(req, res, []);
 });
@@ -39,15 +40,15 @@ function getMediaUrls(req, res, result, maxId) {
             return;
         }
         if (tweet.length >= 1) {
-            var parsed = parseTweet(tweet, req.query.tags.split(','));
+            var parsed = parseTweet(tweet, req.query.tags.split(','), req.query.from);
             if (parsed.maxId == maxId) {
                 res.send(result);
                 return;
             }
 
-            result = result.concat(req.query.ov
-                ? parsed.data.filter(x => x.video)
-                : parsed.data);
+            result = result.concat(req.query.ov ?
+                parsed.data.filter(x => x.video) :
+                parsed.data);
             if (result && result.length > req.query.limit) {
                 res.send(result);
                 return;
@@ -57,10 +58,10 @@ function getMediaUrls(req, res, result, maxId) {
     });
 }
 
-function parseTweet(tweet, tags) {
+function parseTweet(tweet, tags, from) {
     var matrix = tweet.filter(x => tags && tags[0] == '' ? true :
             tags.some(z => x.entities.hashtags.map(y => y.text).indexOf(decodeURIComponent(z)) >= 0))
-        .filter(x => x.extended_entities && !moment(x.created_at).isBefore(moment().subtract(1, 'months'))).map(x =>
+        .filter(x => x.extended_entities && !moment(x.created_at).isBefore(moment().subtract(from, 'months'))).map(x =>
             x.extended_entities.media.map(y => {
                 var videoInfo = y.type == 'video' ? y.video_info.variants.filter(z => z.bitrate) : null;
                 return {
